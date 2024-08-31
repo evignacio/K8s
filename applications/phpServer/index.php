395a0d88ec4b931ba9cac604eece836f668cda88
evignacio <reignomo27@hotmail.com>
2024-08-31 20:24:54 -0300

    Criando configMap

--- applications/phpServer/index.php
+++ applications/phpServer/index.php
@@ -1,1 +1,5 @@
-<?php echo "Server UP - V3 - Welcome ". getenv("name", true) ." !!" ?>
+<?php
+    echo "Server UP - V3 - Welcome ". getenv("name", true) ." !!";
+
+    echo "My family is: ". file_get_contents('/var/www/html/file/myfamily.txt');
+?>

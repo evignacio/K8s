77f025df8d6ce8c0cabbaa6b9c90c219847cf264
evignacio <reignomo27@hotmail.com>
2024-09-08 19:52:37 -0300

    Criando secret

--- applications/phpServer/index.php
+++ applications/phpServer/index.php
@@ -1,5 +1,7 @@
 <?php
-    echo "Server UP - V3 - Welcome ". getenv("name", true) ." !!";
-
-    echo "My family is: ". file_get_contents('/var/www/html/file/myfamily.txt');
+    header('Content-type: text/plain');
+    
+    echo "Server UP - V3 - Welcome ". getenv("name", true) ." !! \n";
+    echo "My family is: ". file_get_contents('/var/www/html/file/myfamily.txt'). "\n" ;
+    echo "That's my secret content: Username ". getenv("username"). ", Password ". getenv("password")
 ?>
